2fe03a0550b5d60a81d98fb01586529a183bfefe
mikeyboy213 <mikeyboy213@de-Leon-PC>
2014-10-07 22:05:54 +0800

    arrows are now spawning properly

--- KeyboardSmasher/src/keyboardsmasher/MainGame.java
+++ KeyboardSmasher/src/keyboardsmasher/MainGame.java
@@ -10,6 +10,7 @@ import com.golden.gamedev.GameEngine;
 import com.golden.gamedev.GameObject;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.util.ArrayList;
 import snakegame.Block;
 
 /**
@@ -20,10 +21,10 @@ public class MainGame extends GameObject{
 
     private int spawnValue;
     private Block main;
-    private Block up;
-    private Block down;
-    private Block left;
-    private Block right;
+    private ArrayList <Block> up = new ArrayList<>();
+    private ArrayList <Block> down = new ArrayList<>();
+    private ArrayList <Block> left = new ArrayList<>();
+    private ArrayList <Block> right = new ArrayList<>();
     private double dimension;
     
     
@@ -34,25 +35,13 @@ public class MainGame extends GameObject{
     @Override
     public void initResources() {
         dimension = 100;
-        
-        up = new Block(getImage("resources/Up.png"),270,740);
-        down = new Block(getImage("resources/Down.png"),270,-100);
-        right = new Block(getImage("resources/Right.png"),-100,270);
-        left = new Block(getImage("resources/Left.png"),740,270);
         main = new Block(getImage("resources/Main.png"),245,245);
     }
 
     @Override
     public void update(long l) {
-        if(System.currentTimeMillis() % 20 == 0) //Determines how fast the arrows show
-            spawnValue = (int)(Math.random()*4);
-        
-        movement(spawnValue);
-        
-        down.update(l);
-        up.update(l);
-        left.update(l);
-        right.update(l);
+        //if(System.currentTimeMillis() % 20 == 0) //Determines how fast the arrows show
+            Generate();
         
     }
 
@@ -62,13 +51,65 @@ public class MainGame extends GameObject{
         gd.fillRect(0, 0, getWidth(), getHeight());
         
         main.render(gd);
-        up.render(gd);
-        down.render(gd);
-        left.render(gd);
-        right.render(gd);
+        
+        for (int ctr = 0; ctr < up.size();ctr++)
+            up.get(ctr).render(gd);
+        
+        for (int ctr = 0; ctr < down.size();ctr++)
+            down.get(ctr).render(gd);
+        
+        for (int ctr = 0; ctr < left.size();ctr++)
+            left.get(ctr).render(gd);
+        
+        for (int ctr = 0; ctr < right.size();ctr++)
+            right.get(ctr).render(gd);
+        
     }
     
-    public void movement(int num){
+    public void Generate(){
+        spawnValue = (int)(Math.random()*4);
+        
+        if(spawnValue == 0){
+            up.add(new Block(getImage("resources/Up.png"),270,740));
+            Movement();
+        }
+        
+        else if(spawnValue == 1){
+            down.add(new Block(getImage("resources/Down.png"),270,-100));
+            Movement();
+        }
+        
+        else if(spawnValue == 2){
+            right.add(new Block(getImage("resources/Right.png"),-100,270));
+            Movement();
+        }
+        
+        else if(spawnValue == 3){
+            left.add(new Block(getImage("resources/Left.png"),740,270));
+            Movement();
+        }
+    }
+    
+    public void Movement(){
+        for(int ctr = 0; ctr < up.size(); ctr++){
+            up.get(ctr).setY(up.get(ctr).getY() - 0.5*dimension);
+        }
+        
+        for(int ctr = 0; ctr < down.size(); ctr++){
+            down.get(ctr).setY(down.get(ctr).getY() + 0.5*dimension);
+        }
+        
+        for(int ctr = 0; ctr < left.size(); ctr++){
+            left.get(ctr).setX(left.get(ctr).getX() - 0.5*dimension);
+        }
+        
+        for(int ctr = 0; ctr < right.size(); ctr++){
+            right.get(ctr).setX(right.get(ctr).getX() + 0.5*dimension);
+        }
+        
+    }
+    
+    /*public void movement(int num){
         if(num == 0 && down.getY()!= 640){
             down.setY(down.getY() + .05*dimension);
         }else if(num ==1 && up.getY()!= 0){
@@ -78,5 +119,5 @@ public class MainGame extends GameObject{
         }else if(num ==3 && right.getX()!= 640){
             right.setY(right.getX() + .05*dimension);
         }
-    }    
+    }*/  
 }
