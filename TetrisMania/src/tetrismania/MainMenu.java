efc377a25a50bbfccf475428d391e086a803bca7
Lester Chong <lestercharles486@yahoo.com>
2014-10-21 19:02:32 +0800

    update

--- TetrisMania/src/tetrismania/MainMenu.java
+++ TetrisMania/src/tetrismania/MainMenu.java
@@ -11,6 +11,7 @@ import com.golden.gamedev.GameObject;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
+import java.util.Random;
 
 /**
  *
@@ -19,13 +20,24 @@ import java.awt.event.KeyEvent;
 public class MainMenu extends GameObject{
 
     private Block block;
+    private int[][] matrix;
+    private int x, y;
+    
     public MainMenu(GameEngine ge) {
         super(ge);
     }
 
     @Override
     public void initResources() {
-        block = new Block(getImage("resources/square.png"), 320, 0);
+        matrix = new int[64][64];
+        
+        for(int ctr = 0; ctr < 64; ctr ++){
+            for(int ctr2 = 0; ctr2 < 64; ctr2++)
+                matrix[ctr][ctr2] = 0;
+        }
+        
+        block = new Block(getImage("resources/line.png"), 320, 0);
+        
     }
 
     @Override
@@ -64,4 +76,60 @@ public class MainMenu extends GameObject{
         return false;
     }
     
+    public void rotate(Block block){
+        
+    }
+    
+    public int generateBlock(){
+        int generated;
+        Random rand = new Random();
+        generated = rand.nextInt()%7;
+        
+        return generated;
+    }
+    
+    public void miniRender(int piece){
+        if(piece == 0){
+            matrix[0][30]=1;
+            matrix[0][31]=1;
+            matrix[0][32]=1;
+            matrix[0][33]=1;
+        }else if(piece == 1){
+            matrix[0][30]=1;
+            matrix[1][30]=1;
+            matrix[1][32]=1;
+            matrix[1][31]=1;
+        }else if(piece == 2){
+            matrix[0][30]=1;
+            matrix[1][30]=1;
+            matrix[1][29]=1;
+            matrix[1][28]=1;
+        }else if(piece == 3){
+            matrix[0][30]=1;
+            matrix[0][31]=1;
+            matrix[1][30]=1;
+            matrix[1][31]=1;
+        }else if(piece == 4){
+            matrix[1][31]=1;
+            matrix[1][32]=1;
+            matrix[0][32]=1;
+            matrix[0][33]=1;
+        }else if(piece == 5){
+            matrix[0][32]=1;
+            matrix[1][32]=1;
+            matrix[1][31]=1;
+            matrix[1][33]=1;
+        }else if(piece == 6){
+            matrix[0][32]=1;
+            matrix[0][31]=1;
+            matrix[1][32]=1;
+            matrix[1][33]=1;
+        }
+    }
+    
+    public void blockMovement(int piece){
+        if(piece == 0 /*&& clicked left*/){
+            
+        }
+    }
 }
